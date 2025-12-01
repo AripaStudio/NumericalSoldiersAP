55a3a72ad24116d623240b46294758f8eb582027
khashayar <aripastudio44@gmail.com>
2025-11-30 23:44:40 -0800

    Create ClassFile with Flatbuffers and Build

---
+++ NumericalSoldiersAP/Service/BuildRandomData.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameConfig;
+using Google.FlatBuffers;
+
+namespace NumericalSoldiersAP.Service
+{
+    public class BuildRandomData
+    {
+        public static VectorOffset CreateRandomBuildingTypeVector(FlatBufferBuilder builder)
+        {
+            const int NUM_BUILDINGS = 4;
+            Offset<BuildingType>[] buildingOffsets = new Offset<BuildingType>[NUM_BUILDINGS];
+
+            Random random = new Random();
+
+            for (short i = 0; i < NUM_BUILDINGS; i++)
+            {
+                TypeOfCapability capability = (TypeOfCapability)random.Next(0, 2);
+
+                Offset<BuildingType> buildingOffset = BuildingType.CreateBuildingType(builder,
+                    id: i,
+                    type_of_capability: capability);
+
+                buildingOffsets[i] = buildingOffset;
+            }
+
+            return builder.CreateVectorOfTables(buildingOffsets);
+        }
+    }
+}
---
+++ NumericalSoldiersAP/Service/DataBuilder.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameConfig;
+using Google.FlatBuffers;
+
+namespace NumericalSoldiersAP.Service
+{
+    public class DataBuilder
+    {
+        public static void BuildGameData()
+        {
+            FlatBufferBuilder builder = new FlatBufferBuilder(1024);
+
+            Offset<GameConstants> constantsOffset = GameConstants.CreateGameConstants(builder,
+                soldier_health_factor: 0.1f,
+                shooter_troop_factor: 0.2f,
+                num_paths: 2,
+                building_defense_multiplier: 2,
+                max_capacity_player: 3000,
+                min_capacity_palyer: 500, 
+                max_capacity_enemy: 3500,
+                min_capacity_enemy: 800,
+                building_typesOffset:BuildRandomData.CreateRandomBuildingTypeVector(builder));
+
+            builder.Finish(constantsOffset.Value);
+
+            string outputPath = "game_config.bin";
+
+            byte[] buffer = builder.SizedByteArray();
+
+            File.WriteAllBytes(outputPath, buffer);
+
+
+        }
+    }
+}
